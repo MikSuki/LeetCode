5de93c0ca1fada6fa6d2de121776f72c79b9f7e7
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-08-18 08:45:58 +0800

    Update 1615.ts

--- medium/1615.ts
+++ medium/1615.ts
@@ -3,27 +3,21 @@
  */
 
 function maximalNetworkRank(n: number, roads: number[][]): number {
-    const G: Map<number, Set<number>> = new Map()
-    const add = (G: Map<number, Set<number>>, u: number, v: number) => {
-        if (G.has(u))
-            G.get(u).add(v)
-        else
-            G.set(u, new Set([v]))
-    }
+    const degree: number[] = new Array(n).fill(0)
+    const G: number[][] = new Array(n).fill(0).map(() => new Array(n).fill(0))
     roads.forEach((edge: number[]) => {
         const [u, v] = edge
-        add(G, u, v)
-        add(G, v, u)
+        ++degree[u]
+        ++degree[v]
+        G[u][v] = 1
+        G[v][u] = 1
     })
 
     let res = 0
     for (let i = 0; i < n; ++i) {
         for (let j = i + 1; j < n; ++j) {
-            const [si, sj] = [G.get(i), G.get(j)]
-            let sum = 0
-            if (si) sum += si.size
-            if (sj) sum += sj.size
-            if (si && si.has(j)) sum -= 1
+            let sum = degree[i] + degree[j]
+            if (G[i][j]) sum -= 1
             res = Math.max(res, sum)
         }
     }
