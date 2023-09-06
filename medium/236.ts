2a6c77a013bce49a6322b386e07ab1a1d4138ec6
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-09-06 09:02:25 +0800

    Create 236.ts

---
+++ medium/236.ts
@@ -0,0 +1,27 @@
+/**
+    236. Lowest Common Ancestor of a Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if (root == null) return null
+    if (root == p || root == q) return root
+    const l = lowestCommonAncestor(root.left, p, q)
+    const r = lowestCommonAncestor(root.right, p, q)
+    if (l == null) return r
+    if (r == null) return l
+    return root
+};
