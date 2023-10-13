8e75fb953fee879cb1ac2b39842f71777d6474f7
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-10-13 13:56:02 +0800

    Update 45.cpp

--- medium/45.cpp
+++ medium/45.cpp
@@ -1,14 +1,26 @@
+/**
+    # 45. Jump Game II
+*/
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jumps = 0, cur_end = 0, cur_far = 0;
-        for(int i = 0; i < nums.size() - 1; ++i){
-            cur_far = max(cur_far, i + nums[i]);
-            if(i == cur_end){
-                cur_end = cur_far;
-                ++jumps;
+        int n = nums.size();
+        if(n == 1) return 0;
+        if(nums[0] >= n - 1) return 1;
+
+        int step = 1;
+        int next = nums[0];
+        int lm = 0;
+
+        for(int i = 1; i < n; ++i){
+            if(i + nums[i] >= n - 1) return step + 1;
+            lm = max(lm, i + nums[i]);
+            if(i == next){
+                step++;
+                next = lm;
             }
         }
-        return jumps;
+        
+        return -1;
     }
 };
