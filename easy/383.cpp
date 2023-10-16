1a653b560863767b39ead1156e02ae07ce3f988a
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-10-16 13:29:21 +0800

    Create 383.cpp

---
+++ easy/383.cpp
@@ -0,0 +1,16 @@
+/**
+    383. Ransom Note
+*/
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr[26];
+        for(int i = 0; i < 26; ++i)
+            arr[i] = 0;
+        for(char c : magazine)
+            arr[c - 'a']++;
+        for(char c : ransomNote)
+            if(--arr[c - 'a'] < 0) return false;
+        return true;
+    }
+};
