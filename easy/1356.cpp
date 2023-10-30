53cf333888d7ed3744eed2be8a440b21cbcd9a97
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-10-31 01:42:30 +0800

    Create 1356.cpp

---
+++ easy/1356.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    static bool cmp(int a, int b){
+        int ba = __builtin_popcount(a);
+        int bb = __builtin_popcount(b);
+
+        if(ba == bb) return a < b;
+        return ba < bb;
+    }
+
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), cmp);
+        return arr;
+    }
+};
